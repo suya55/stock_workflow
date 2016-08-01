13a0b190dc8b048fe33259db6481e2628c427e3e
Castor <castor.k@daumkakao.com>
2016-08-01 18:36:38 +0900

    python display format modified

--- stock.py
+++ stock.py
@@ -23,10 +23,10 @@ elems = []
 locale.setlocale(locale.LC_ALL, 'en_US')
 for item in obj["result"]["d"]:
   if item["cv"] > 0 :
-    arrow = "\033[0;31m▲\033[0m"
+    arrow = "\033[0;31m▲"
   elif item["cv"] == 0 :
     arrow = "-"
   else :
-    arrow = "\033[0;34m▼\033[0m"
-  print u'- %s : %s(%s%s, %0.2f%%) : http://finance.daum.net/item/main.daum?code=%s' % (item["nm"], locale.format("%d", item["nv"], grouping=True), arrow, locale.format("%d", item["cv"], grouping=True), item["cr"], item["cd"])
+    arrow = "\033[0;36m▼"
+  print u'| %s | \033[0;32m%s\033[0m원(%s%0.2f%%\033[0m %s원) | 거래량 : %s | 전날종가 : %s원 | http://finance.daum.net/item/main.daum?code=%s |' % (item["nm"], locale.format("%d", item["nv"], grouping=True), arrow, item["cr"], locale.format("%d", item["cv"], grouping=True), locale.format("%d", item["aq"], grouping=True), locale.format("%d", item["pcv"], grouping=True),item["cd"])
   
